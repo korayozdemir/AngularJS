2faa97b8ed28a3617175b879719367ff73dfb877
korayozdemir <koray_ozdemir@msn.com>
2020-05-28 21:10:06 +0300

    Add project files.

---
+++ AngularJS/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using AngularJS.Models;
+
+namespace AngularJS.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+        public IActionResult AngularModule()
+        {
+            return View();
+        }
+
+    }
+}
